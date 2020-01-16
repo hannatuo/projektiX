1c613c5f72cf4f85cfaa6fd73d789c332a763ecb
Hanna Tuomi <hannamtuomi@gmail.com>
2020-01-16 15:30:58 +0200

    eka commit

---
+++ src/Topic.java
@@ -0,0 +1,13 @@
+import java.time.LocalDate;
+
+public class Topic {
+    private int id;
+    private String title;
+    private String description;
+    private String additionalSource;
+    private boolean complete;
+    private LocalDate creationDate;
+    private LocalDate completionDate;
+
+    }
+
